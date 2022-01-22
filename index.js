806262ee2d5cdf9f12734fa00af9723cc9801c1d
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-22 11:48:54 -0500

    function to reverse chronological notes

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+function transformNotes(notes) {
+  return notes.slice().sort(function (a, b) {
+    return b.timestamp - a.timestamp;
+  });
+}
+
 function formatTimestamp(timestamp) {
   return new Date(timestamp).toUTCString();
 }
 
 var notes = [
-  { id: 1, body: "This is a first test ", timestamp: Date.now() },
-  { id: 2, body: "This is a second test", timestamp: Date.now() },
-  { id: 3, body: "This is a third test ", timestamp: Date.now() },
+  { id: 1, body: "This is a first test ", timestamp: Date.now() - 300000000 },
+  { id: 2, body: "This is a second test", timestamp: Date.now() - 200000000 },
+  { id: 3, body: "This is a third test ", timestamp: Date.now() - 100000000 },
   { id: 4, body: "This is a fourth test ", timestamp: Date.now() },
 ];
 
 var htmlString = "";
-notes.forEach(function (note) {
+transformNotes(notes).forEach(function (note) {
   htmlString += `
     <div class="note-selector">
       <p class="note-selector-title">${note.body}</p>
