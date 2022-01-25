6b7baa1800ed4414ec56fb59b6f78157e383c9de
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-24 20:48:56 -0500

    add note body and timestamp in note editor on note selector click

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ function selectNote($note) {
     $activeNote.classList.remove("active");
   }
   $note.classList.add("active");
+
+  document.querySelector(".note-editor-input").value = $note.dataset.body;
+  document.querySelector(".note-editor-info").innerHTML = formatTimestamp(parseInt($note.dataset.timestamp));
 }
 
 var notes = [
@@ -38,7 +41,12 @@ var notes = [
 var htmlString = "";
 transformNotes(notes).forEach(function (note) {
   htmlString += `
-    <div class="note-selector" onClick="selectNote(this)">
+    <div 
+      class="note-selector" 
+      onClick="selectNote(this)" 
+      data-body="${note.body}"
+      data-timestamp="${note.timestamp}"
+      >
       <p class="note-selector-title">${formatTitle(note.body)}</p>
       <p class="note-selector-timestamp">${formatTimestamp(note.timestamp)}</p>
     </div>
