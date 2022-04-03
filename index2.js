24b3655c4d0570f40e98b363c9b2e52ca88df4d7
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-04-03 12:29:35 -0400

    add methods to sort and format notes

--- index2.js
+++ index2.js
@@ -1,5 +1,26 @@
 /* global $ */
 
+function transformNotes(notes) {
+  return notes.slice().sort(function (a, b) {
+    return b.timestamp - a.timestamp;
+  });
+}
+
+function formatTimestamp(timestamp) {
+  return new Date(timestamp).toUTCString();
+}
+
+function formatTitle(body) {
+  var maxLength = 20;
+  if (body.length > maxLength) {
+    return body.substring(0, maxLength - 3) + "...";
+  } else if (body.length === 0) {
+    return "New Note";
+  } else {
+    return body;
+  }
+}
+
 var notes = [
   { id: 1, body: "This is a first test", timestamp: Date.now() - 300000000 },
   { id: 2, body: "This is a second test this is a very long note", timestamp: Date.now() + 200000000 },
@@ -7,11 +28,11 @@ var notes = [
   { id: 4, body: "This is a fourth test", timestamp: Date.now() },
 ];
 
-notes.forEach(function (note) {
+transformNotes(notes).forEach(function (note) {
   var $noteSelector = $(`
   <div class="note-selector">
-    <p class="note-selector-title">${note.body}</p> 
-    <p class="note-selector-timestamp">${note.timestamp}</p> 
+    <p class="note-selector-title">${formatTitle(note.body)}</p> 
+    <p class="note-selector-timestamp">${formatTitle(note.timestamp)}</p> 
   </div>
   `);
   $(".note-selectors").append($noteSelector);
